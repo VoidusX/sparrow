6c2fe1116f858f2b50cfb3a95ce584b7ce7d2856
Voidus X <voidus-git@mail.voidus.dev>
2026-07-22 00:52:41 -0700

    chore: update applications list

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,7 +16,9 @@ set -ouex pipefail
 export CARGO_HOME="/tmp/cargo"
 mkdir -p "$CARGO_HOME"
 
-dnf5 install -y git cargo
+dnf5 remove -y firefox firefox-langpacks htop nwg-panel nvtop
+
+dnf5 install -y git cargo btop
 
 dnf5 -y copr enable lionheartp/Hyprland
 dnf5 install -y hyprland noctalia noctalia-greeter neovim greetd kitty distrobox
@@ -26,11 +28,26 @@ dnf5 -y copr enable imput/helium
 dnf5 install -y helium-bin
 dnf5 -y copr disable imput/helium
 
+# File Manager (A)
+dnf5 -y copr enable relativesure/all-packages
+dnf5 install -y superfile --setopt=install_weak_deps=False
+dnf5 -y copr disable relativesure/all-packages
+
+# File Manager (B)
+# This is a alternative for advanced users specifically.
+dnf5 -y copr enable lihaohong/yazi
+dnf5 install -y yazi --setopt=install_weak_deps=False
+dnf5 -y copr disable lihaohong/yazi
+
 git clone https://github.com/Gerharddc/litterbox.git /tmp/litterbox
 cd /tmp/litterbox/litterbox
 cargo build --release
 cp target/release/litterbox /usr/bin/
 cd /
+
+# Pre-installed games for simple entertainment
+cargo install tetro-tui
+cargo install chess-tui
 
 # Use a COPR Example:
 #
